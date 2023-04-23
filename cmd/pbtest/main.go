19f359e0f54da37c72a2efdd8c15044bd22ae9a7
zcong1993 <zhangcong1992@gmail.com>
2023-04-23 19:22:58 +0800

    chore: pb test

---
+++ cmd/pbtest/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/zcong1993/grpc-go-beyond/pb"
+)
+
+func main() {
+	for i := 0; i < 10000; i++ {
+		a := &pb.TestRequest{
+			Message: "test",
+			Name:    "xxx",
+		} // 下面不加空行 space count=2, 加一行空行 space count=1
+
+		fmt.Printf("%d, message: %s, space count:%d \n", i, a.String(), strings.Count(a.String(), " "))
+	}
+}
